a6914671c34740f42eb578be226a4d3438407e3a
SidKarthik <44774604+SidKarthik1437@users.noreply.github.com>
2020-11-06 23:41:51 +0530

    small update to sidebar

--- slack-clone/src/Sidebar.js
+++ slack-clone/src/Sidebar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Sidebar.css';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <h2>Sid Karthik</h2>
-                <h3>
-                    <FiberManualRecordIcon />
-                    SidKarthik
-                </h3>
+                <div className="sidebar__info">
+                    <h2>Heyaaa</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                        Sid Karthik
+                    </h3>
+                </div>
+                <CreateIcon />
             </div>
         </div>
     )
