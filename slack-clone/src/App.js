189afe7de78bfa3e7e04a5ef412c901c75358662
Sid Karthi_main <sa268592@gmail.com>
2020-11-06 19:38:08 +0530

    Added Header Icons

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,12 +1,16 @@
+import React from 'react'
 import './App.css';
+import  Header  from './Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Heyaaaaaaaaaaaaaa</h1>
 
       {/* Header */}
-      {/* This is fun af */}
+      <Header />
+      {/* Sidebar */}
+      {/* React-Router -> Chat screen */}
       
     </div>
   );
