2934543b03cf6b04f7dc72ff8115570a316d1e0f
Sid Karthi_main <sa268592@gmail.com>
2020-11-06 16:30:24 +0530

    Initial

---
+++ slack-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Heyaaaaaaaaaaaaaa</h1>
+
+      {/* Header */}
+      {/* Dimbadakkummi */}
+
+    </div>
+  );
+}
+
+export default App;
